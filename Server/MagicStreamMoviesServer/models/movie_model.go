15f65057e48068b6137395e021e9b51f6ade6e8c
yukno1 <121278340+yukno1@users.noreply.github.com>
2026-03-09 16:41:24 +0800

    add user model

--- Server/MagicStreamMoviesServer/models/movie_model.go
+++ Server/MagicStreamMoviesServer/models/movie_model.go
@@ -2,16 +2,19 @@ package models
 
 import "go.mongodb.org/mongo-driver/v2/bson"
 
+// Genre struct
 type Genre struct {
 	GenreID   int    `bson:"genre_id" json:"genre_id" validate:"required"`
 	GenreName string `bson:"genre_name" json:"genre_name" validate:"required,min=2,max=100"`
 }
 
+// Ranking struct
 type Ranking struct {
 	RankingValue int    `bson:"ranking_value" json:"ranking_value" validate:"required"`
 	RankingName  string `bson:"ranking_name" json:"ranking_name" validate:"required"`
 }
 
+// Movie struct
 type Movie struct {
 	ID          bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
---
+++ Server/MagicStreamMoviesServer/models/user_model.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+// User struct
+type User struct {
+	ID             bson.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID         string        `json:"user_id" bson:"user_id"`
+	FirstName      string        `json:"first_name" bson:"first_name" validate:"required,min=2,max=100"`
+	LastName       string        `json:"last_name" bson:"last_name" validate:"required,min=2,max=100"`
+	Email          string        `json:"email" bson:"email" validate:"required,email"`
+	Password       string        `json:"password" bson:"password" validate:"required,min=6,max=100"`
+	Role           string        `json:"role" bson:"role" validate:"oneof=ADMIN USER"`
+	CreateAt       time.Time     `json:"create_at" bson:"create_at"`
+	UpdateAt       time.Time     `json:"update_at" bson:"update_at"`
+	Token          string        `json:"token" bson:"token"`
+	RefreshToken   string        `json:"refresh_token" bson:"refresh_token"`
+	FavouriteGenre []Genre       `json:"favourite_genre" bson:"favourite_genre" validate:"requied,dive"`
+}
